fa0d7a9df4696889b64d7244e1681a9200c22ad8
DenisPopkov <popkov-den03@mail.ru>
2024-03-13 09:06:49 +0600

    remove: parcelable

--- features/main/domain/src/main/java/ru/popkov/marvelapp/features/main/domain/model/HeroCard.kt
+++ features/main/domain/src/main/java/ru/popkov/marvelapp/features/main/domain/model/HeroCard.kt
@@ -1,35 +1,29 @@
 package ru.popkov.marvelapp.features.main.domain.model
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
 @Serializable
-@Parcelize
 data class HeroData(
     val code: Int,
     val status: String,
     val data: HeroResult
-) : Parcelable
+)
 
 @Serializable
-@Parcelize
 data class HeroResult(
     val results: List<HeroCard>
-) : Parcelable
+)
 
 @Serializable
-@Parcelize
 data class HeroCard(
     val id: Int,
     val name: String,
     val description: String,
     val thumbnail: HeroThumbnail,
-) : Parcelable
+)
 
 @Serializable
-@Parcelize
 data class HeroThumbnail(
     val path: String,
     val extension: String,
-) : Parcelable
+)
